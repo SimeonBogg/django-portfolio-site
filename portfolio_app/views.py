f51fd2189ab94b7766e8c7ce48848d4b21429e45
Simeon Bogg <simeonbogg@Simeons-Air.Home>
2023-11-20 21:17:25 +0000

    urls and template setup

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'portfolio_app/home.html')
+
+def posts(request):
+    return render(request, 'portfolio_app/posts.html')
+
+def post(request):
+    return render(request, 'portfolio_app/post.html')
+
+def profile(request):
+    return render(request, 'portfolio_app/profile.html')
+
+
